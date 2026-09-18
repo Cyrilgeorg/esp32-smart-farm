ff74128bd8b3fcb7bbdacef2a6e11fa0891d82bf
Cyrilgeorg <123372691+Cyrilgeorg@users.noreply.github.com>
2026-09-18 20:15:50 +0300

    app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ app.add_middleware(
 HF_TOKEN = os.getenv("HF_TOKEN")
 
 client = InferenceClient(
-    model="Qwen/Qwen2.5-32B-Instruct",
+    model="Qwen/Qwen2.5-7B-Instruct",
     provider="auto",
     api_key=HF_TOKEN,
 )
